d478055189efdfd92e7f38e8aa00541a512ea133
edjimenezs <ed.jimenez.saa@gmail.com>
2025-09-25 13:45:07 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { useSocket } from '../contexts/SocketContext'
+import { useSocket } from '/contexts/SocketContext'
 import Head from 'next/head'
 
 interface Message {
@@ -149,6 +149,7 @@ export default function Home() {
     </>
   )
 }
+
 
 
 
